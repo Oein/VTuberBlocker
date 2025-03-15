65b9c43afcf7c6dd0106ae5316f2f02d0e69deb8
Oein <oein0219@gmail.com>
2025-03-16 01:51:38 +0900

    fix url

--- deploy/only.js
+++ deploy/only.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // 유저스크립트 다운로드 URL
   downloadButton.addEventListener("click", () => {
-    const downloadUrl = "https://vblock.oein.kr/dist-orig.user.js";
+    const downloadUrl = "https://vblock.oein.kr/dist-only.user.js";
     window.open(downloadUrl, "_blank");
   });
 });
